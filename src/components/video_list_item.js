be3fb2646c2ef2711663871b536ded1453ac203e
Cody Stammer <stammer.3@osu.edu>
2017-05-06 18:45:55 -0400

    added thumbnail/title to videoListItem

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-    return <li>Videos</li>
+const VideoListItem = ({video}) => {
+    //cost video = props.videos;
+    const imageUrl = video.snippet.thumbnails.default.url;
+    return (
+        <li className="list-group-item">
+            <div className="vedeo-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
