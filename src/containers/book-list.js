9f87d6271ba025f31ca9e0e7eac077cf70a82f95
Cody <stammer.3@osu.edu>
2017-05-07 17:56:47 -0400

    finished section 4, reducers used in detail to populate data

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import {selectBook} from '../actions/index';
+import { bindActionCreators} from 'redux'; //makes sure action flows through reducers
 
 
 class BookList extends Component{
@@ -9,12 +11,13 @@ class BookList extends Component{
             return (
                 <li 
                 key={book.title} 
+                onClick={() => this.props.selectBook(book)}
                 className="list-group-item">{book.title}</li>);
         });
     }
 
     render(){
-        console.log(this.props.books);
+        //console.log(this.props.books);
         return(
             <ul className="list-group col-sm-4">
                 {this.renderList()}
@@ -30,5 +33,13 @@ function mapStateToProps(state){
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch){
+    //Whenever selectBook is called, the result should be passed to all our reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+//Promote BookList from a component to a container - it needs to know about this new dispatch
+//method, selectBook. Make it available as a prop
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 //takes function and component to make container
