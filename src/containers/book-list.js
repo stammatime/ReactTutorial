9e72dcb38ff59d68fdbfaa8612671202c735e9fb
Cody <stammer.3@osu.edu>
2017-05-07 13:06:00 -0400

    first reducer is working

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+
+class BookList extends Component{
+
+    renderList(){
+        return this.props.books.map((book) => {
+            return (
+                <li 
+                key={book.title} 
+                className="list-group-item">{book.title}</li>);
+        });
+    }
+
+    render(){
+        console.log(this.props.books);
+        return(
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    //whatever is returned will show up as props inside of BookList
+    return {
+         books: state.books
+    };
+}
+
+export default connect(mapStateToProps)(BookList);
+//takes function and component to make container
--- src/index.js
+++ src/index.js
@@ -1,47 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
-import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_list';
-import VideoDetail from './components/video_detail';
-import _ from 'lodash';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-const API_KEY = "AIzaSyCf27eNNWNW8qZP_LIV_QD2idGmAF0QxJA";
+import App from './components/app';
+import reducers from './reducers';
 
-//create a new component. This component should produce some html
-class App extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-        this.videoSearch('cats');
-}
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-videoSearch(term){
-    YTSearch({key: API_KEY, term: term}, (videos) => {
-        this.setState({
-            videos: videos,
-            selectedVideo: videos[0]
-        });
-    });
-}
-
-    render(){
-        //returns new function that can only be called every 300ms
-        const videoSearch = _.debounce((term) => {this.videoSearch(term) }, 300);
-        return (
-        <div>
-            <SearchBar onSearchTermChange = {videoSearch}/>
-            <VideoDetail video={this.state.selectedVideo}/>
-            <VideoList 
-                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                videos={this.state.videos}/>
-        </div>
-        );
-    }
-}
-
-//Take this component's generated html and put it in the DOM
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
