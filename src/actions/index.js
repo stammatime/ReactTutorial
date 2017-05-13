08eff8cef01d63da8753a032792e55dc5c77edfa
Cody <stammer.3@osu.edu>
2017-05-12 22:30:52 -0400

    Added axios to dependencies

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '26b210909c283fa231951155e097095a';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+export function fetchWeather(city){
+    const url = `${ROOT_URL}&q=${city},us`;
+    return{
+        type: FETCH_WEATHER
+    };
+}
