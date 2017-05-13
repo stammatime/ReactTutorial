89bf02a85e508683d2a82c1ad00e9e3e45918294
Cody <stammer.3@osu.edu>
2017-05-13 02:47:00 -0400

    API call working

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '26b210909c283fa231951155e097095a';
-const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
