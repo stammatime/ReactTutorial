7097f6412570b6d9da1098bb3385d5e627623d53
Cody <stammer.3@osu.edu>
2017-05-13 02:11:46 -0400

    added payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '26b210909c283fa231951155e097095a';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-
+//action creator 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
     return{
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
