7e5bc0afbe3bc57b2a4ff5856ab76a44090b2387
Cody <stammer.3@osu.edu>
2017-05-17 14:52:19 -0400

    added form to new post page

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,46 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
+
+    renderField(field){
+        return(
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                     //pass eventhandlers to input props
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
+
     render(){
         return(
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field 
+                    name="title"
+                    component={this.renderField}
+                    label="Title"
+                />
+                <Field 
+                    name="tags"
+                    component={this.renderField}
+                    label="Tags"
+                />
+                <Field 
+                    name="Content"
+                    component={this.renderField}
+                    label="Post Content"
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
