af438b84f26e133e6689664e4c61d1daa603a37d
Cody <stammer.3@osu.edu>
2017-05-17 22:42:43 -0400

    Added form validation, navigation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
 
 class PostsNew extends Component {
 
     renderField(field){
+        const { meta: {touched,error}} = field;
+        const className  = `form-group ${touched && error ? 'has-danger' : ''}`;
         return(
-            <div className="form-group">
+            <div className={className}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
@@ -13,34 +16,65 @@ class PostsNew extends Component {
                      //pass eventhandlers to input props
                     {...field.input}
                 />
+                <div className="text-help">{field.meta.touched? field.meta.error: ""}</div>
             </div>
         );
     }
 
+    onSubmit(values){
+        console.log(values);
 
+    }
     render(){
+        const { handleSubmit} = this.props;
+
         return(
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field 
                     name="title"
                     component={this.renderField}
                     label="Title"
                 />
                 <Field 
-                    name="tags"
+                    name="categories"
                     component={this.renderField}
-                    label="Tags"
+                    label="Categories"
                 />
                 <Field 
-                    name="Content"
+                    name="content"
                     component={this.renderField}
                     label="Post Content"
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
             </form>
         );
     }
 }
 
+function validate(values){
+    //console.log(values) -> {title:"aoeu",categories:"aoeu",content:"aoeu"}
+    const errors = {};
+
+    //Validate the inputs from 'values'
+    if(!values.title){
+        errors.title = "Enter a title";
+    }
+    if(!values.categories){
+        errors.categories = "Enter a category!";
+    }
+    if(!values.content){
+        errors.content = "Enter a content!";
+    }
+
+    //if errors is empty, then form is fine to submit
+    //if errors has any properties, redux form assumes form is invalid
+    return errors;
+
+}
+
 export default reduxForm({
+    //same as validate:validate
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
