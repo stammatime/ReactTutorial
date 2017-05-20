39584f07ae347e6b014e9c482180ae7df7e02978
Cody <stammer.3@osu.edu>
2017-05-20 18:42:11 -0400

    added fetch post action creator and updated reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,15 @@
 import _ from 'lodash';
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST} from '../actions';
+
 
 export default function(state = {}, action){
     switch(action.type){
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case FETCH_POST:
+            const post = action.payload.data
+            //take all state and return it with new key/val pair
+            return {...state, [action.payload.data.id]: action.payload.data]}
         default:
             return state;
     }
