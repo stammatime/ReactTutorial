83d507084405b08e43d5de8d9555b5034826eff6
Cody <stammer.3@osu.edu>
2017-05-20 23:34:43 -0400

    delete post added. App complete.

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,15 +1,19 @@
 import _ from 'lodash';
-import {FETCH_POSTS, FETCH_POST} from '../actions';
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 
 export default function(state = {}, action){
     switch(action.type){
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
         case FETCH_POST:
-            const post = action.payload.data
             //take all state and return it with new key/val pair
-            return {...state, [action.payload.data.id]: action.payload.data]}
+            //const post = action.payload.data
+            //newState[post.id] = post;
+            //return newState
+            return {...state, [action.payload.data.id]: action.payload.data}
         default:
             return state;
     }
